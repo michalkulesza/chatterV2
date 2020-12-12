e681e60bdef9cf9a3e630c5df384c5cc804ded27
Michal <kulesza.michal@gmail.com>
2020-12-12 20:32:22 +0100

    Conditional route rendering

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-import { Login } from './pages';
+import React from "react";
+import { useSelector } from "react-redux";
+import { Redirect, Route, Switch } from "react-router-dom";
+import { Login, Chat } from "./pages";
+import { RootState } from "./redux/reducers/rootReducer";
+import { HOME, CHAT } from "./constants/routes";
 
 const App: React.FC = () => {
-  return (
-    <Route path="/">
-      <Login />
-    </Route>
-  );
+	const auth = useSelector((state: RootState) => state.auth);
+	return (
+		<Switch>
+			<Route exact path="/">
+				{auth.username ? <Redirect to={CHAT} /> : <Login />}
+			</Route>
+			<Route exact path="/chat">
+				{auth.username ? <Chat /> : <Redirect to={HOME} />}
+			</Route>
+			<Route path="/">
+				<Redirect to={HOME} />
+			</Route>
+		</Switch>
+	);
 };
 
 export default App;
---
+++ client/src/pages/Chat/Chat.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Chat.scss";
+
+interface Props {}
+
+const Chat: React.FC<Props> = () => {
+	return (
+		<div className="Chat">
+			<h1>Chat</h1>
+		</div>
+	);
+};
+
+export default Chat;
