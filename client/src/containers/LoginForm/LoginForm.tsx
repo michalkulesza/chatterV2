91f530b1e33338f80c3fcd5ca401aa3ed2d0d1c4
Michal <kulesza.michal@gmail.com>
2020-12-12 12:21:03 +0100

    Added LoginForm with login as a guest

--- client/src/containers/LoginForm/LoginForm.tsx
+++ client/src/containers/LoginForm/LoginForm.tsx
@@ -1,50 +1,69 @@
-import React, { useState } from 'react';
-import './LoginForm.scss';
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { loginWithoutPassword } from "../../redux/actions/auth";
+import "./LoginForm.scss";
 
-import FormInput from './FormInput/FormInput';
-import FormButton from './FormButton/FormButton';
-import FormError from './FormError/FormError';
+import FormInput from "./FormInput/FormInput";
+import FormButton from "./FormButton/FormButton";
 
 interface Props {}
 
-const LoginForm: React.FC<Props> = () => {
-  const [user, setUser] = useState('');
-  const [password, setPassword] = useState('');
-  const [checkbox, setCheckbox] = useState(false);
-  const [error, setError] = useState('');
+let timer: NodeJS.Timeout;
 
-  return (
-    <div className="LoginForm">
-      <div className="Container">
-        <div className="Logo">Chatter</div>
-        <div className="Title">Join In</div>
-        <form>
-          <FormInput
-            type="text"
-            setState={setUser}
-            state={user}
-            label="Username"
-            required
-          />
-          <FormInput
-            type="password"
-            setState={setPassword}
-            state={password}
-            label="Password (optional)"
-            required={checkbox}
-          />
-          <FormInput
-            type="checkbox"
-            setState={setCheckbox}
-            state={checkbox}
-            label="I want to register Username"
-          />
-          <FormError error={error} />
-          <FormButton>Join</FormButton>
-        </form>
-      </div>
-    </div>
-  );
+const LoginForm: React.FC<Props> = () => {
+	const dispatch = useDispatch();
+	const [user, setUser] = useState("");
+	const [password, setPassword] = useState("");
+	const [register, setRegister] = useState(false);
+	const [error, setError] = useState("");
+
+	useEffect(() => {
+		timer = setTimeout(() => setError(""), 2000);
+
+		return () => clearTimeout(timer);
+	}, [error]);
+
+	const hasLength = (string: string, length: number) => string.length >= length;
+
+	const validName = (name: string) => hasLength(name, 3) && name !== "admin";
+
+	const validPassword = (password: string) => hasLength(password, 4);
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		const passwordEntered = password.length > 0;
+
+		if (!validName(user)) return setError("Username must be at least 3 characters long.");
+		if (!validPassword(password) && (register || passwordEntered))
+			return setError("Password must be at least 4 characters long.");
+
+		if (passwordEntered && register) return console.log("REGISTER");
+		if (passwordEntered) return console.log("LOGIN WITHOUT PASSWORD");
+		dispatch(loginWithoutPassword(user));
+	};
+
+	return (
+		<div className="LoginForm">
+			<div className="Container">
+				<div className="Logo">Chatter</div>
+				<div className="Title">Join In</div>
+				<form onSubmit={e => handleSubmit(e)}>
+					<FormInput type="text" setState={setUser} state={user} label="Username" required />
+					<FormInput
+						type="password"
+						setState={setPassword}
+						state={password}
+						label="Password (optional)"
+						required={register}
+					/>
+					<FormInput type="checkbox" setState={setRegister} state={register} label="I want to register Username" />
+					<div className="error">{error}</div>
+					<FormButton>Join</FormButton>
+				</form>
+			</div>
+		</div>
+	);
 };
 
 export default LoginForm;
