722a5870fd86eed6ec8a8d22f908e78519b40754
Michal <kulesza.michal@gmail.com>
2020-12-12 22:29:42 +0100

    + Login route

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -32,4 +32,40 @@ router.post("/register", async (req, res) => {
 	}
 });
 
+router.post("/login", async (req, res) => {
+	try {
+		const { username, password } = req.body;
+		const userExists = await UserModel.exists({ username });
+
+		if (userExists) {
+			const user = await UserModel.findOne({ username });
+			const passwordCorrect = await bcrypt.compare(password, user.password);
+
+			if (passwordCorrect) {
+				res.sendStatus(200);
+				//USER JOINED
+			} else {
+				res.status(401).send("Incorrect password");
+			}
+		} else {
+			res.status(404).send("User does not exist.");
+		}
+	} catch (error) {
+		res.status(400).send("Something went wrong.");
+	}
+});
+
+router.post("/join", async (req, res) => {
+	try {
+		const { username } = req.body;
+		const userExists = await UserModel.exists({ username });
+
+		if (userExists) return res.status(401).send("Username is taken. Please Login.");
+
+		res.sendStatus(200);
+	} catch (error) {
+		res.status(400).send("Something went wrong.");
+	}
+});
+
 module.exports = router;
