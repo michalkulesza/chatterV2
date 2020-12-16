2355ba2e7b5ae7ba56a8be058b1a75e3a8ce3502
Michal <kulesza.michal@gmail.com>
2020-12-16 15:46:45 +0100

    Added skeleton of Sidebar

--- client/src/containers/Sidebar/Sidebar.tsx
+++ client/src/containers/Sidebar/Sidebar.tsx
@@ -1,12 +1,18 @@
 import React, { useState } from "react";
 import "./Sidebar.scss";
 
+import { SearchBar } from "../../containers";
+
 interface Props {}
 
 const Sidebar: React.FC<Props> = () => {
 	const [collapsed, setCollapsed] = useState(false);
 
-	return <div className={`sidebar ${collapsed ? "collapsed" : ""}`}></div>;
+	return (
+		<div className={`sidebar ${collapsed ? "collapsed" : ""}`}>
+			<SearchBar />
+		</div>
+	);
 };
 
 export default Sidebar;
