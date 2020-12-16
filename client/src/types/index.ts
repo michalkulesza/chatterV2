13030ab224fa16cf5f602b3d4dc57e2cdf7a1ee9
Michal <kulesza.michal@gmail.com>
2020-12-16 20:07:33 +0100

    Added types

---
+++ client/src/types/index.ts
@@ -0,0 +1,19 @@
+export interface MessageI {
+	_id: string;
+	author: string;
+	content: string;
+	created: string;
+	seenBy: UserI[];
+}
+
+export interface UserI {
+	_id: string;
+	name: string;
+}
+
+export interface RoomI {
+	name: string;
+	type: "room" | "private";
+	users?: UserI[];
+	messages?: MessageI[];
+}
