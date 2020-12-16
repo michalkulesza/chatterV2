1969ff24446b1283edcc452933905740d45241c8
Michal <kulesza.michal@gmail.com>
2020-12-16 20:15:55 +0100

    Added Icon

--- client/src/containers/List/List.tsx
+++ client/src/containers/List/List.tsx
@@ -3,6 +3,8 @@ import "./List.scss";
 
 import { User, Room } from "../../components";
 import { UserI, RoomI } from "../../types";
+import { FiUsers } from "react-icons/fi";
+import { RiChat1Line } from "react-icons/ri";
 
 interface Props {
 	type: "users" | "rooms";
@@ -13,7 +15,10 @@ interface Props {
 const List: React.FC<Props> = ({ type, usersData, roomsData }) => {
 	return (type === "users" && usersData) || (type === "rooms" && roomsData) ? (
 		<div className="list">
-			<h4 className="title">{type === "users" ? "Users" : "Rooms"}</h4>
+			<h4 className="title">
+				<div className="icon">{type === "users" ? <FiUsers /> : <RiChat1Line />}</div>
+				{type === "users" ? "Users" : "Conversations"}
+			</h4>
 			{type === "users" && usersData && usersData?.map((user: UserI) => <User key={user._id} name={user.name} />)}
 			{type === "rooms" && roomsData && roomsData?.map((room: RoomI) => <Room key={room.name} name={room.name} />)}
 		</div>
