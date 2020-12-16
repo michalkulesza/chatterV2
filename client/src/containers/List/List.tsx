f078780a69c0ed34a620f003865a730daa2fe02d
Michal <kulesza.michal@gmail.com>
2020-12-16 21:16:22 +0100

    List is now collapsable

--- client/src/containers/List/List.tsx
+++ client/src/containers/List/List.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import "./List.scss";
 
 import { User, Room } from "../../components";
 import { UserI, RoomI } from "../../types";
+
 import { FiUsers } from "react-icons/fi";
 import { RiChat1Line } from "react-icons/ri";
+import { BiChevronDown } from "react-icons/bi";
 
 interface Props {
 	type: "users" | "rooms";
@@ -13,14 +15,37 @@ interface Props {
 }
 
 const List: React.FC<Props> = ({ type, usersData, roomsData }) => {
+	const listRef = useRef<HTMLDivElement>(null);
+	const [collapsed, setCollapsed] = useState(false);
+
+	const handleCollapseButton = () => setCollapsed(!collapsed);
+
+	const style = {
+		transition: "all 0.4s cubic-bezier(0.23, 1, 0.32, 1)",
+	};
+
+	const collapsedStyle = {
+		transition: "all 0.4s cubic-bezier(0.23, 1, 0.32, 1);",
+		marginTop: `-${listRef?.current?.clientHeight}px`,
+		pointerEvents: "none",
+		opacity: "0",
+	};
+
 	return (type === "users" && usersData) || (type === "rooms" && roomsData) ? (
 		<div className="list">
-			<h4 className="title">
-				<div className="icon">{type === "users" ? <FiUsers /> : <RiChat1Line />}</div>
-				{type === "users" ? "Users" : "Conversations"}
-			</h4>
-			{type === "users" && usersData && usersData?.map((user: UserI) => <User key={user._id} name={user.name} />)}
-			{type === "rooms" && roomsData && roomsData?.map((room: RoomI) => <Room key={room.name} name={room.name} />)}
+			<header>
+				<h4 className="title">
+					<div className="icon">{type === "users" ? <FiUsers /> : <RiChat1Line />}</div>
+					{type === "users" ? "Users" : "Conversations"}
+				</h4>
+				<button className={`collapseButton ${collapsed && "active"}`} onMouseDown={handleCollapseButton}>
+					<BiChevronDown />
+				</button>
+			</header>
+			<div className="main" style={collapsed ? collapsedStyle : style} ref={listRef}>
+				{type === "users" && usersData && usersData?.map((user: UserI) => <User key={user._id} name={user.name} />)}
+				{type === "rooms" && roomsData && roomsData?.map((room: RoomI) => <Room key={room.name} name={room.name} />)}
+			</div>
 		</div>
 	) : null;
 };
