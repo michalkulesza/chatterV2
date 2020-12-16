f3c23b69fa752a5c84a12d83fcb97cfc63d40c9c
Michal <kulesza.michal@gmail.com>
2020-12-16 23:11:23 +0100

    Updated styling

--- client/src/components/Button/Button.tsx
+++ client/src/components/Button/Button.tsx
@@ -4,13 +4,16 @@ import "./Button.scss";
 import { CgSpinner } from "react-icons/cg";
 
 interface Props {
-	loading?: boolean;
 	children: any;
+	loading?: boolean;
+	type?: "square" | "rectangle";
+	color?: "defaultColor" | "transparent";
+	svgSize?: "large";
 }
 
-const Button: React.FC<Props> = ({ loading, children }) => {
+const Button: React.FC<Props> = ({ children, loading, type = "rectangle", color = "defaultColor", svgSize }) => {
 	return (
-		<button className="Button" disabled={loading}>
+		<button className={`button ${type} ${color} ${svgSize}`} disabled={loading}>
 			{loading === true ? (
 				<span>
 					<CgSpinner />
