56c204511a6b764402b3f5d26ee4816394e2e7c5
Michal <kulesza.michal@gmail.com>
2020-12-19 20:00:02 +0100

    Registered badge next to username

--- client/src/components/User/User.tsx
+++ client/src/components/User/User.tsx
@@ -2,17 +2,28 @@ import React from "react";
 import "./User.scss";
 
 import { Badge } from "../../components";
+import { IoShieldCheckmarkSharp } from "react-icons/io5";
 
 interface Props {
 	currentUser: string;
 	name: string;
+	handler: any;
+	registered: boolean;
 }
 
-const User: React.FC<Props> = ({ currentUser, name }) => {
+const User: React.FC<Props> = ({ currentUser, name, handler, registered }) => {
+	const handleUserClick = () => currentUser !== name && registered && handler(currentUser, name);
+
 	return (
-		<div className="user">
+		<div className="user" onMouseDown={handleUserClick}>
 			{currentUser === name ? `${name} (You)` : name}
-			<Badge color="green">Online</Badge>
+			<span>
+				<div className="icon">
+					<IoShieldCheckmarkSharp />
+					<div className="label">Registered</div>
+				</div>
+				<Badge color="green">Online</Badge>
+			</span>
 		</div>
 	);
 };
