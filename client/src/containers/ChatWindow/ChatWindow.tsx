0fb2ad3b15b1ec17d396b6ea243535394aa823d7
Michal <kulesza.michal@gmail.com>
2020-12-20 22:09:34 +0100

    Chat info when locked and no messages

--- client/src/containers/ChatWindow/ChatWindow.tsx
+++ client/src/containers/ChatWindow/ChatWindow.tsx
@@ -1,27 +1,48 @@
-import React, { useState } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../redux/reducers/rootReducer";
-import "./ChatWindow.scss";
 
 import { Message } from "../../containers";
-import { MessageI } from "../../types";
+
+import { RiChat1Line } from "react-icons/ri";
+import { IoLockClosed } from "react-icons/io5";
+import "./ChatWindow.scss";
 
 interface Props {}
 
 const ChatWindow: React.FC<Props> = () => {
 	const messages = useSelector((state: RootState) => state.room.messages);
 	const currentUser = useSelector((state: RootState) => state.auth.username);
+	const locked = useSelector((state: RootState) => state.room.locked);
 
 	return (
 		<div className="chatWindowContainer">
 			<div className="chatWindow">
-				{messages && currentUser && messages.length > 0 ? (
+				{locked ? (
+					<div className="notice">
+						<div className="icon">
+							<IoLockClosed />
+						</div>
+						<div className="text">Chat had been locked.</div>
+						<div className="text">
+							To continue chatting and keep your messages saved
+							<br />
+							make sure that both of users are registered!
+						</div>
+					</div>
+				) : currentUser && messages?.length > 0 ? (
 					messages.map((message, i) => {
 						const prevMessage = i > 0 ? messages[i - 1] : undefined;
 						return <Message key={message._id} currentUser={currentUser} message={message} prevMessage={prevMessage} />;
 					})
 				) : (
-					<h3>No messages</h3>
+					<div className="notice">
+						<div className="icon">
+							<RiChat1Line />
+						</div>
+						<div className="text">No messages yet</div>
+						<div className="text">Be a first one to start the conversation!</div>
+					</div>
 				)}
 			</div>
 		</div>
