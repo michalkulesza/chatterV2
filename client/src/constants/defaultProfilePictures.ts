2eaba4c4bffe99573391ad065af36e5d2f7a1248
Michal <kulesza.michal@gmail.com>
2020-12-22 21:35:44 +0100

    Static profile images

---
+++ client/src/constants/defaultProfilePictures.ts
@@ -0,0 +1,6 @@
+export const Default1 =
+	"https://firebasestorage.googleapis.com/v0/b/chatter-ce304.appspot.com/o/images%2Fdefault1.png?alt=media&token=bd2d4723-0445-42d0-8b43-9ba43f45de8d";
+export const Default2 =
+	"https://firebasestorage.googleapis.com/v0/b/chatter-ce304.appspot.com/o/images%2Fdefault2.jpg?alt=media&token=7f7ce10e-8e1e-46fb-a78f-b032a56f4f4f";
+export const Default3 =
+	"https://firebasestorage.googleapis.com/v0/b/chatter-ce304.appspot.com/o/images%2Fdefault3.jpg?alt=media&token=8fd6c947-d723-4c05-98ce-e4b204696ee5";
