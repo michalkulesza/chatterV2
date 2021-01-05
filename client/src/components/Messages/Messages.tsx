5e4fc9dd39435fac93eafd494d5f808569d90905
Michal <kulesza.michal@gmail.com>
2021-01-05 21:43:34 +0100

    Responmsive styling of main chat

--- client/src/components/Messages/Messages.tsx
+++ client/src/components/Messages/Messages.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { MessageI } from "../../types";
 import { Message } from "../../containers";
 import "./Messages.scss";
@@ -9,11 +9,16 @@ interface Props {
 }
 
 const Messages: React.FC<Props> = ({ messages, isDisabled }) => {
+	const [currentDay, setCurrentDay] = useState<number>(0);
+	const [currentMonth, setCurrentMonth] = useState<number>(0);
+	const [currentYear, setCurrentYear] = useState<number>(0);
+
 	return (
 		<div className="messages">
 			{messages.map((message, i) => {
 				const prevMessage = i > 0 ? messages[i - 1] : undefined;
 				const nextMessage = i < messages.length ? messages[i + 1] : undefined;
+				const spacer = new Date(message.created).getDay() > currentDay;
 				return (
 					<Message
 						key={message._id}
