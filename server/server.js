100a146c6bfe0f1c4e4376b7beb5114c621996f2
Michal <kulesza.michal@gmail.com>
2021-01-09 22:17:30 +0100

    Server NODE.ENV for CORS

--- server/server.js
+++ server/server.js
@@ -9,12 +9,13 @@ const authRouter = require("./routes/authRouter");
 require("./db");
 
 const PORT = process.env.PORT || 8888;
+const ORIGIN = process.env.NODE_ENV === "production" ? "https://xcxz-chatter.netlify.app" : "http://localhost:3000";
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(function (_req, res, next) {
-	res.setHeader("Access-Control-Allow-Origin", "https://xcxz-chatter.netlify.app");
+	res.setHeader("Access-Control-Allow-Origin", ORIGIN);
 	res.setHeader("Access-Control-Allow-Methods", "GET, POST");
 	res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type,Authorization");
 	next();
